89b447b60fb6ccf40c2b384f30f17857322caa05
des98 <dev.desertcod98@proton.me>
2023-08-01 22:54:16 +0200

    in /category/ made Link only on asset thumbnail and name

--- app/components/Asset.tsx
+++ app/components/Asset.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import Link from "next/link";
 import Button from "./Button";
 
-type Asset = InferModel<typeof assets> & {author: {name: string}};
+type Asset = InferModel<typeof assets> & { author: { name: string } };
 
 export default async function Asset(asset: Asset) {
   const thumbnailUrl = asset.thumbnailPath
@@ -15,24 +15,26 @@ export default async function Asset(asset: Asset) {
     : "/assets/noImage.svg";
 
   return (
-    <Link
-      className="flex flex-col h-60 w-60  rounded overflow-hidden"
-      href={"/asset/" + asset.id}
-    >
-      <div className="relative w-60 aspect-video overflow-hidden">
+    <div className="flex flex-col h-60 w-60  rounded overflow-hidden">
+      <Link
+        className="relative w-60 aspect-video overflow-hidden"
+        href={"/asset/" + asset.id}
+      >
         <Image
           src={thumbnailUrl}
           alt={asset.name}
           fill
           className="object-cover"
         />
-      </div>
+      </Link>
       <div className="flex flex-col mt-2 px-3">
         <span className="font-extralight text-sm">{asset.author.name}</span>
-        <span className="font-medium text-lg">{asset.name}</span>
+        <Link href={"/asset/" + asset.id} className="w-fit">
+          <span className="font-medium text-lg">{asset.name}</span>
+        </Link>
         <span>TODO stars</span>
         <div className="flex flex-row w-full justify-between items-center">
-          <span>{asset.priceCents/100} $</span>
+          <span>{asset.priceCents / 100} $</span>
           <Button>
             <span className="text-lg">+</span>
             <Image
@@ -44,6 +46,6 @@ export default async function Asset(asset: Asset) {
           </Button>
         </div>
       </div>
-    </Link>
+    </div>
   );
 }
