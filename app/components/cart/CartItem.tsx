69036fdf2c06d92e82aae26d072a65614889745e
des98 <dev.desertcod98@proton.me>
2023-08-18 19:39:31 +0200

    images in cart have now fixed dimensions

--- app/components/cart/CartItem.tsx
+++ app/components/cart/CartItem.tsx
@@ -7,12 +7,15 @@ import AddToCart from "../AddToCart";
 export default function CartItem({cartItem}: {cartItem: CartItem}){
     return (
         <div className="flex flex-row justify-between items-center w-full border-2 p-4 rounded-xl border-gray-300">
+          <div className="w-[120px] h-[100px] relative">
             <Image
-                src={cartItem.asset.thumbnailUrl || "/assets/noImage.png"}
-                alt={cartItem.asset.name}
-                width={100}
-                height={100}
-            />
+                  src={cartItem.asset.thumbnailUrl || "/assets/noImage.png"}
+                  alt={cartItem.asset.name}
+                  fill
+                  className="object-cover rounded"
+              />
+          </div>
+           
             <span>{cartItem.asset.name}</span>
             <div className = "flex flex-col items-center gap-2">
                 <span>{cartItem.price/100} $</span>
