7031886ec21a3ab999bf4b483d2a7dd335781276
des98 <dev.desertcod98@proton.me>
2023-08-21 18:07:38 +0200

    basic /admin work done

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,22 @@
-export default function Admin(){
+import db from "@/db"
+
+export default async function Admin(){
+  let unModeratedAssets = await db.query.assets.findMany({
+    with: {
+      assetImages: true,
+      assetModeration: true,
+    }
+  })
+
+  unModeratedAssets = unModeratedAssets.filter(asset => asset.assetModeration.state === "PENDING");
+
   return (
-    <div>
-      admin
-    </div>
+    <ul>
+      {unModeratedAssets.map(asset => {
+        return (
+          <li key = {asset.id}>{asset.name}</li>
+        )
+      })}
+    </ul>
   )
 }
