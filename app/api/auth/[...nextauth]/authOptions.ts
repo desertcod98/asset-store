9d079a02e249113089492dae530a186c70254a95
des98 <dev.desertcod98@proton.me>
2023-08-22 19:52:22 +0200

    restored middleware functionality

--- app/api/auth/[...nextauth]/authOptions.ts
+++ app/api/auth/[...nextauth]/authOptions.ts
@@ -56,5 +56,38 @@ export const authOptions : NextAuthOptions = {
       clientSecret: process.env.GITHUB_SECRET as string,
     }),
   ],
+  callbacks: {
+    async session({ token, session }) {
+      if (token) {
+        session.user.id = token.id;
+        session.user.name = token.name;
+        session.user.email = token.email;
+        session.user.image = token.picture;
+      }
+
+      return session;
+    },
+    async jwt({ token, user }) {
+      const [dbUser] = await db
+        .select()
+        .from(users)
+        .where(eq(users.email, token.email || ""))
+        .limit(1);
+
+      if (!dbUser) {
+        if (user) {
+          token.id = user?.id;
+        }
+        return token;
+      }
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        email: dbUser.email,
+        picture: dbUser.image,
+        role: dbUser.role,
+      };
+    },
+  }
 };
 
